82e0f2431999cd576dfe3a84b497f9c2794b3ed9
Thiago2ATI <169170488+Thiago2ATI@users.noreply.github.com>
2024-11-25 15:35:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quando a escravidão foi banida no Brasil?',
+    '13 de maio de 1888'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokyo'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o nome dado a um alelo que, quando presente em doses simples no genótipo, determina o fenótipo?',
+    'dominante'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz "Como está você?" em Inglês?',
+    '"Como está você?" em ingles é "How are you?" (how uh yoo)'
+)
